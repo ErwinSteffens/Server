1e8ced48fdb693a5c3e76557f9b031ae929a1174
Erwin Steffens <erwinsteffens@gmail.com>
2016-12-08 21:59:39 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp');
+var rsync = require('gulp-rsync');
+
+var paths = {
+  configs: ['traefik/**/*'],
+  compose: 'docker-compose.yml'
+};
+
+var host = 'erwinsteffens.com'; 
+
+// Copy app
+gulp.task('copy', ['copy-config', 'copy-compose']);
+
+// Copy configuration files
+gulp.task('copy-config', function() {
+  gulp.src(paths.configs)
+    .pipe(rsync({
+      hostname: host,
+      username: 'root',
+      destination: '/usr/share/'
+    }));
+});
+
+// Copy compose file
+gulp.task('copy-compose', function() {
+  gulp.src(paths.compose)
+    .pipe(rsync({
+      hostname: host,
+      username: 'root',
+      destination: '/root/server'
+    }));
+});
+
+// Watch for file changes
+gulp.task('watch', function() {
+  gulp.watch(paths.configs, ['copy-config']);
+  gulp.watch(paths.compose, ['copy-compose']);
+});
+
+gulp.task('default', ['watch', 'copy']);
